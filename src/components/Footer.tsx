6940755bfc4ebca6090cf8c2bd5e708c95e9fb8b
mlyshol <32914556+mlyshol@users.noreply.github.com>
2025-08-30 19:52:57 -0700

    Fix promise issues on detail pages

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,7 @@
 export default function Footer() {
   return (
-    <>
     <footer className="bg-neutral text-white text-center py-6 text-sm">
       © {new Date().getFullYear()} The Spirit Hub — All rights reserved.
-       <ins className="CANBMDDisplayAD" data-bmd-ad-unit="1219220520250620T142018403D8CD860AA710400BA7BB613989C34002" style={{display:"block"}}></ins>
-        <script src="https://secureaddisplay.com/au/bmd/"></script>
     </footer>
-    </>
   );
 }
